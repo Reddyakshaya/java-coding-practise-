abf5076f78baa16dd17f4b03045fbddb4d92fbae
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-16 10:06:15 +0530

    Added Swapping

---
+++ Day 1/Swap.java
@@ -0,0 +1,13 @@
+public class Swap {
+    public static void main(String[]args){
+        int a=1;
+        int b=2;
+        System.out.println("Before swapping:"+"a:"+a+" "+"b:"+b);
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("After swapping:" +"+a:"+a+" "+"b" +b);
+
+    }
+    
+}

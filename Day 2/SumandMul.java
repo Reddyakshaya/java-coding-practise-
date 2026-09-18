d20b0b468017f9151f54f27bde9f46f357215517
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-18 20:02:24 +0530

    Sum and mul

---
+++ Day 2/SumandMul.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+public class SumandMul {
+    public static void main(String[]args){
+        Scanner sc=new Scanner(System.in);
+        System.out.println("Enter a and b numbers:");
+        int a=sc.nextInt();
+        int b=sc.nextInt();
+        System.out.println("sum"+(a+b));
+          System.out.println("mul"+(a*b));
+
+
+    }
+
+    
+}

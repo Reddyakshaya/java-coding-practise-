21747e07c0c1bbd6f8c50e1ded8ffb79900165ac
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-19 12:40:11 +0530

    Even or Odd

---
+++ Day 3/EvenOdd.java
@@ -0,0 +1,11 @@
+public class EvenOdd {
+    public static void main(String[]args){
+        int n=32;
+        if(n%2==0){
+            System.out.print("Even");
+        }else{
+            System.out.print("Odd");
+        }
+    }
+    
+}

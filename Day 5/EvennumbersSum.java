db5cd61f97a7c0a4b267b32c6d00a09ba8fe106c
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-20 20:13:06 +0530

    Sum of Even numbers

---
+++ Day 5/EvennumbersSum.java
@@ -0,0 +1,11 @@
+public class EvennumbersSum {
+    public static void main(String[]args){
+        int n=10;
+        int sum=0;
+        for(int i=2;i<=10;i+=2){
+            sum=sum+i;
+            System.out.print("Sum of even numbers:"+sum);
+        }
+    }
+    
+}

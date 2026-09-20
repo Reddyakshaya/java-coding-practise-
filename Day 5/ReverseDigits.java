91e54eea8948f91fd65561a5ccf785e90832f322
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-20 21:46:41 +0530

    Reverse Digit

---
+++ Day 5/ReverseDigits.java
@@ -0,0 +1,12 @@
+public class ReverseDigits {
+    public static void main(String[]args){
+        int n=12345;
+        int reverse=0;
+        while(n>0){
+            int digit=n%10;
+            reverse=reverse*10+digit;
+            n=n/10;
+        }System.out.print(reverse);
+    }
+    
+}

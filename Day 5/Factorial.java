dd05d6d91bfa066f048b079a72b3fa7261710450
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-20 20:16:53 +0530

    Factorial

---
+++ Day 5/Factorial.java
@@ -0,0 +1,13 @@
+public class Factorial {
+    public static void main(String[]args){
+        int n=10;
+        int temp=1;
+        for(int i=10;i>=1;i--){
+            temp=temp*i;
+            System.out.print("Factorial of number is:" +temp);
+
+        }
+
+    }
+    
+}

2305c05576bb90836c66d7989fae19161c62e1e7
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-25 19:05:10 +0530

    Even Addition

---
+++ Day 9/EvenSum.java
@@ -0,0 +1,14 @@
+public class EvenSum {
+    public static void main(String[]args){
+        int [][] arr={ {1,2,3},{4,5,6},{7,8,9}};
+        int sum=0;
+        for(int i=0;i<arr.length;i++){
+            for(int j=0;j<arr[i].length;j++){
+                if(arr[i][j]%2==0){
+                  sum+=arr[i][j];
+                }
+            }
+        }
+        System.out.println("Sum of even numbers:"+sum);
+    }
+}

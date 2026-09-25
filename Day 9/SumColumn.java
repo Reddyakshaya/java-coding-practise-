5cd87a2edb3d69c07119d8c68f62f8eb9248fa28
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-25 18:43:47 +0530

    Column Sum

---
+++ Day 9/SumColumn.java
@@ -0,0 +1,15 @@
+public class SumColumn {
+    public static void main(String[]args){
+        int arr[][]={{1,2,3},{4,5,6},{7,8,9}};
+        int row=arr.length;
+        int column=arr[0].length;
+        for(int j=0;j<column;j++){
+            int sum=0;
+            for(int i=0;i<row;i++){
+                  sum+=arr[i][j];
+            }
+             System.out.println("column"+(j+1)+"="+sum);
+        }
+    }
+    
+}

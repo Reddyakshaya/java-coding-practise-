09b6b316fdd4a4d2a93528ae8f139691098dae47
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-25 19:18:39 +0530

    transpose

---
+++ Day 9/Transpose.java
@@ -0,0 +1,20 @@
+public class Transpose {
+    public static void main(String[]args){
+        int arr[][]={ {1,2,3},{4,5,6},{7,8,9}};
+        int row=arr.length;
+        int column=arr[0].length;
+         int transpose[][] = new int[column][row];
+           for(int i=0;i<row;i++){
+              for(int j=0;j<column;j++){
+               transpose[j][i]=arr[i][j];
+        }
+    }
+               for(int i=0;i<transpose.length;i++){
+              for(int j=0;j<transpose[i].length;j++){
+                System.out.print(transpose[i][j]+" ");
+              }
+              System.out.println();
+               }
+
+    }
+}

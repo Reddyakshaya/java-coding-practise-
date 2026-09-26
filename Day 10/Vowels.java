027f38f1a560858b1e22628949076c18c336c44a
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-26 19:36:09 +0530

    Counting vowels

---
+++ Day 10/Vowels.java
@@ -0,0 +1,14 @@
+public class Vowels {
+    public static void main(String[]args){
+        String str="Programing";
+        int count =0;
+        for(int i=0;i<str.length();i++){
+            char ch=str.charAt(i);
+            if(ch=='a'||ch=='e'||ch=='i'||ch=='o'||ch=='u'){
+                count++;
+            }
+        }
+         System.out.print("Vowels:"+count);
+    }
+    
+}

c55b8f05cdee638f3adc42bd73ad3bf840dda370
Reddy Akshaya <reddyakshaya47@gmail.com>
2026-09-26 19:43:11 +0530

    cases

---
+++ Day 10/cases.java
@@ -0,0 +1,7 @@
+public class cases {
+      public static void main(String[]args){
+        String str="level";
+        System.out.println(str.toUpperCase());
+        System.out.println(str.toLowerCase());
+      }
+}
